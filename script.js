8af77eee94eb882c68fb39cb447ece6f8cce9ccf
SiddhiKanakiya <siddhikanakiya@gmail.com>
2022-12-24 10:22:54 +0530

    Solution

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+let inputText = document.getElementById("evaluatedText");
+let resultLength = document.getElementById("letterCount");
+
+inputText.addEventListener("keyup",stringLength);
+
+function stringLength(){
+    let s = inputText.value;
+    let sLength = s.length;
+
+    resultLength.innerText = sLength;
+    
+}
